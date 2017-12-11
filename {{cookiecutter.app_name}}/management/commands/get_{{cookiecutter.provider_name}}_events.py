b6e558eeeecb0ce38c4b30cecefcf30b7b7b2d87
Sergio Tomas <sergio.tomas@feverup.com>
2017-12-11 17:08:58 +0100

    Update template

---
+++ {{cookiecutter.app_name}}/management/commands/get_{{cookiecutter.provider_name}}_events.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""This command generates and stores Ingresso events from the ingresso client.
+"""
+from __future__ import absolute_import, unicode_literals, print_function
+
+from django.core.management.base import BaseCommand
+
+from {{cookiecutter.app_name}}.services import {{cookiecutter.provider_name_capitalized}}Service
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write('Getting Ingresso Events...')
+
+        return {{cookiecutter.provider_name_capitalized}}Service().sync_events()
