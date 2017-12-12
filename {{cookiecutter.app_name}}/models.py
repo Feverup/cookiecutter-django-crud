efd03b2800495d22db60aab32ea3772295e6c7da
Sergio Tomas <sergio.tomas@feverup.com>
2017-12-12 12:34:46 +0100

    Fix string format

--- {{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.app_name}}/models.py
@@ -135,8 +135,8 @@ class {{cookiecutter.model_name}}(models.Model):
                 )
             else:
                 raw_data = api.post(
-                    '/Accounts/{{{cookiecutter.provider_name}}_sugar_id}/link/accounts_fever_venue'
-                    '_addressses_2/{place_sugar_id}'.format(
+                    '/Accounts/{% raw % }{{% endraw %}{{cookiecutter.provider_name}}{% raw % }_sugar_id}{% endraw %}/link/accounts_fever_venue'
+                    '_addressses_2/{% raw % }{place_sugar_id}{% endraw % }'.format(
                         {{cookiecutter.provider_name}}_sugar_id=settings.{{cookiecutter.provider_name|upper}}_API_SUGAR_CRM_ID,
                         place_sugar_id=place_sugar_id
                     ),
